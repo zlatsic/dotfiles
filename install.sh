d98a80a0978d561a6ee2fd3aa1e29950b85451f5
zlatsic2 <zlatan.sicanica@happening.xyz>
2025-04-10 16:42:10 +0200

    feat: merge

--- install.sh
+++ install.sh
@@ -3,15 +3,7 @@
 dotfiles_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo running for dotfiles dir $dotfiles_dir
 
-ln -sn $dotfiles_dir/alacritty $HOME/.config/alacritty
-
 ln -sn $dotfiles_dir/fish $HOME/.config/fish
-
-ln -sn $dotfiles_dir/i3 $HOME/.config/i3
-
-ln -sn $dotfiles_dir/mutt $HOME/.mutt
-
-ln -sn $dotfiles_dir/polybar $HOME/.config/polybar
 
 ln -sn $dotfiles_dir/tmux $HOME/.tmux
 ln -sn $dotfiles_dir/tmux/tmux.conf $HOME/.tmux.conf
@@ -22,4 +14,20 @@ ln -sn $dotfiles_dir/vim $HOME/.config/nvim
 curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +'PlugInstall --sync' +qa || nvim +'PlugInstall --sync' +qa
+
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	ln -sn $dotfiles_dir/alacritty $HOME/.config/alacritty
+
+	ln -sn $dotfiles_dir/i3 $HOME/.config/i3
+
+	ln -sn $dotfiles_dir/mutt $HOME/.mutt
+
+	ln -sn $dotfiles_dir/polybar $HOME/.config/polybar
+fi
+
+if [ "$uname" == "Darwin" ]; then
+	ln -sn $dotfiles_dir/zsh/zshrc $HOME/.zshrc
+	ln -sn $dotfiles_dir/zsh/system-zshrc $HOME/.system-zshrc
+	git clone ssh://git@github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
+fi
  
